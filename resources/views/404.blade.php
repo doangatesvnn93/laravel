6ad7edefa9dc34bc40704b4c8617563d554b8a35
doangatesvnn93 <doangatesvnn93@gmail.com>
2018-03-18 18:59:55 +0700

    update

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -4,6 +4,10 @@
 @endsection
 @section('content')
     <div class="container">
-        <h1>Có lỗi xảy ra hoặc trang không tồn tại!</h1>
+        <h1 style="margin-bottom: 10px;"><font face="Tahoma" color="red">Error 404</font></small></h1>
+        <h1 style="margin-top: 10px;">Có lỗi xảy ra hoặc trang không tồn tại</h1>
+        <div class="text-center mgb-20">
+            <a href="{{ route('landing') }}" class="btn btn-large btn-info"><i class="icon-home icon-white"></i> Trở về trang chủ</a>
+        </div>
     </div>
 @endsection
