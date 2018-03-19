82b80cdf7ec46b0d82510aa000c6a7f293054cb3
doangatesvnn93 <doangatesvnn93@gmail.com>
2018-03-19 16:15:32 +0700

    update

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends InitController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    public function list(Request $request)
+    {
+        $listCategory = Category::where('status', '<>', null)->paginate(10);
+        $data = array(
+            'listCategory' => $listCategory
+        );
+        return view('admin.category.list', $data);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        if ($request->isMethod('post')) {
+            $name = $request->name ? $request->name : '';
+            $status = $request->status ? $request->status : 1;
+            $data = array(
+                'name' => $name,
+                'status' => $status,
+                'slug' => $this->removeSign($name, '-', true)
+            );
+
+            if ($name) {
+                Category::create($data);
+                $this->setFlashData('success', trans('message.commons.successfully'));
+            } else {
+                return view('admin.category.create', $data);
+            }
+        }
+        return view('admin.category.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $data = Category::where('id', $id)->first();
+        $request = request();
+        if ($request->isMethod('post')) {
+            $name = $request->name ? $request->name : $data->name;
+            $status = $request->status;
+            $obj = array(
+                'name' => $name,
+                'status' => $status,
+                'slug' => $this->removeSign($name, '-', true)
+            );
+
+            if ($name) {
+                Category::where('id', $id)->update($obj);
+                $this->setFlashData('success', trans('message.commons.successfully'));
+                return redirect(route('category-list'));
+            } else {
+                return view('admin.category.edit', ['data' => $data]);
+            }
+        }
+        return view('admin.category.edit', ['data' => $data]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
